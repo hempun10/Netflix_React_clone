432e7ec9fe29ff6ff4ae14ff7807635bce249793
Hem Pun <hemthegentlmen@gmail.com>
2023-04-20 10:48:50 +0545

    Images added and Navbar components added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "../../assets/Logonetflix.png";
+import { Link } from "react-router-dom";
+import { ImSearch } from "react-icons/im";
+const Header = () => {
+  return (
+    <nav className="header">
+      <img src={logo} alt="Logo" />
+      <div className="">
+        <Link to="/shows">Tv Shows</Link>
+        <Link to="/movies">Movies</Link>
+        <Link to="/recent">Recently Added</Link>
+        <Link to="/list">My List</Link>
+      </div>
+      <ImSearch />
+    </nav>
+  );
+};
+
+export default Header;
