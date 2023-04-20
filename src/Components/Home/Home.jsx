551a94b2d9d5540907f2998aa8e7218aef6c79eb
Hem Pun <hemthegentlmen@gmail.com>
2023-04-20 19:52:41 +0545

    All Section and Gnere Added Donw with basic Clone

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from "react";
 import "./Home.scss";
 import Row from "../Row/Row";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import { BiPlay } from "react-icons/bi";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const apiKey = "37b0b3a46dbe8139100d1f7c07069e28";
 const url = "https://api.themoviedb.org/3";
+const imgUrl = "https://image.tmdb.org/t/p/original";
 const upcoming = "upcoming";
 const now_playing = "now_playing";
 const popular = "popular";
@@ -15,6 +19,7 @@ const Home = () => {
   const [nowPlayingMovies, setUpNowPlayingMovies] = useState([]);
   const [popularMovies, setPopularMovies] = useState([]);
   const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [genre, setGenre] = useState([]);
 
   useEffect(() => {
     const fetchUpcoming = async () => {
@@ -41,7 +46,14 @@ const Home = () => {
       } = await axios.get(`${url}/movie/${topRated}?api_key=${apiKey}`);
       setTopRatedMovies(results);
     };
+    const fetchGenre = async () => {
+      const {
+        data: { genres },
+      } = await axios.get(`${url}/genre/movie/list?api_key=${apiKey}`);
+      setGenre(genres);
+    };
 
+    fetchGenre();
     fetchUpcoming();
     fetchNowPlaying();
     fetchPopular();
@@ -49,11 +61,39 @@ const Home = () => {
   }, []);
   return (
     <section className="home">
-      <div className="banner"></div>
+      <div
+        className="banner"
+        style={{
+          background: popularMovies[0]
+            ? `url(${imgUrl}/${popularMovies[0].poster_path})`
+            : "#000",
+        }}
+      >
+        {popularMovies[0] && <h1>{popularMovies[0].original_title}</h1>}
+        {popularMovies[0] && <p>{popularMovies[0].overview}</p>}
+
+        <div>
+          <button>
+            <BiPlay />
+            Play
+          </button>
+          <button>
+            My List <AiOutlinePlus />
+          </button>
+        </div>
+      </div>
       <Row title={"Upcoming Movies"} arr={upcomingMovies} />
       <Row title={"Popular on netflix"} arr={popularMovies} />
       <Row title={"Recently Viewed"} arr={nowPlayingMovies} />
       <Row title={"Top Rated"} arr={topRatedMovies} />
+
+      <div className="genreBox">
+        {genre.map((item, index) => (
+          <Link to={`/genre/${item.id}`} key={index}>
+            {item.name}
+          </Link>
+        ))}
+      </div>
     </section>
   );
 };
